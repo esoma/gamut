3af8c31bc4003d779622ac45dd030ed4ff74f56f
Erik Soma <stillusingirc@gmail.com>
2021-09-12 21:06:28 -0400

    Add mouse buttons.

--- src/gamut/peripheral/_mouse.py
+++ src/gamut/peripheral/_mouse.py
@@ -3,9 +3,14 @@ from __future__ import annotations
 
 __all__ = [
     'Mouse',
+    'MouseButton',
+    'MouseButtonEvent',
+    'MouseButtonPressed',
+    'MouseButtonReleased',
     'MouseConnected',
     'MouseDisconnected',
     'MouseEvent',
+    'MouseMoved',
 ]
 
 # gamut
@@ -16,6 +21,7 @@ from gamut._sdl import sdl_event_callback_map, sdl_window_event_callback_map
 from gamut._window import get_window_from_sdl_id, Window
 # python
 from typing import Any, Optional
+from weakref import ref
 # pysdl2
 from sdl2 import SDL_MOUSEMOTION, SDL_TOUCH_MOUSEID, SDL_WINDOWEVENT_LEAVE
 
@@ -26,6 +32,23 @@ class MouseEvent(PeripheralEvent, peripheral=...):
     @property
     def mouse(self) -> Mouse:
         return self.peripheral
+
+
+class MouseButtonEvent(MouseEvent, peripheral=..., button=...):
+    button: MouseButton
+    is_pressed: bool
+
+
+class MouseButtonPressed(MouseButtonEvent,
+    peripheral=..., button=..., is_pressed=True
+):
+    pass
+
+
+class MouseButtonReleased(MouseButtonEvent,
+    peripheral=..., button=..., is_pressed=False
+):
+    pass
 
 
 class MouseConnected(MouseEvent, PeripheralConnected, peripheral=...):
@@ -41,12 +64,116 @@ class MouseMoved(MouseEvent, peripheral=...):
     window: Optional[Window]
 
 
+class BaseMouseButton:
+
+    left: BaseMouseButton
+    middle: BaseMouseButton
+    right: BaseMouseButton
+    front: BaseMouseButton
+    back: BaseMouseButton
+
+    def __init_subclass__(cls, mouse: Optional[Mouse] = None):
+        super().__init_subclass__()
+        cls.left = cls('left', mouse=mouse)
+        cls.middle = cls('middle', mouse=mouse)
+        cls.right = cls('right', mouse=mouse)
+        cls.front = cls('front', mouse=mouse)
+        cls.back = cls('back', mouse=mouse)
+
+    def __init__(self, name: str, mouse: Optional[Mouse] = None):
+        self._name = name
+        self._mouse: Optional[ref[Mouse]] = ref(mouse) if mouse else None
+
+    def __repr__(self) -> str:
+        identifier = repr(self._name)
+        if self._mouse:
+            mouse = self._mouse()
+            if mouse:
+                identifier = f'{identifier} for {mouse!r}'
+        return f'<gamut.peripheral.MouseButton {identifier}>'
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+
+class MouseButton(BaseMouseButton):
+
+    left: MouseButton
+    middle: MouseButton
+    right: MouseButton
+    front: MouseButton
+    back: MouseButton
+
+    Event: type[MouseButtonEvent]
+    Pressed: type[MouseButtonPressed]
+    Released: type[MouseButtonReleased]
+
+    def __init__(self, name: str, mouse: Optional[Mouse] = None):
+        super().__init__(name, mouse=mouse)
+        if mouse:
+            class EventWithMouse( # type: ignore
+                MouseButtonEvent,
+                mouse.Event, # type: ignore
+                button=self
+            ):
+                pass
+            self.Event = EventWithMouse
+            class PressedWithMouse( # type: ignore
+                MouseButtonPressed,
+                EventWithMouse
+            ):
+                pass
+            self.Pressed = PressedWithMouse
+            class ReleasedWithMouse( # type: ignore
+                MouseButtonReleased,
+                EventWithMouse
+            ):
+                pass
+            self.Released = ReleasedWithMouse
+        else:
+            class EventNoMouse(MouseButtonEvent,
+                peripheral=..., button=self
+            ):
+                pass
+            self.Event = EventNoMouse
+            class PressedNoMouse(MouseButtonPressed, EventNoMouse,
+                peripheral=...
+            ):
+                pass
+            self.Pressed = PressedNoMouse
+            class ReleasedNoMouse(MouseButtonReleased, EventNoMouse,
+                peripheral=...
+            ):
+                pass
+            self.Released = ReleasedNoMouse
+
+
+class PressableMouseButton(MouseButton):
+
+    left: PressableMouseButton
+    middle: PressableMouseButton
+    right: PressableMouseButton
+    front: PressableMouseButton
+    back: PressableMouseButton
+
+    def __init__(self, name: str, mouse: Mouse):
+        super().__init__(name, mouse=mouse)
+        self._is_pressed = False
+
+    @property
+    def is_pressed(self) -> bool:
+        return self._is_pressed
+
+
 class Mouse(Peripheral):
 
     Event: type[MouseEvent]
     Connected: type[MouseConnected]
     Disconnected: type[MouseDisconnected]
     Moved: type[MouseMoved]
+
+    Button: type[PressableMouseButton]
 
     def __init__(self, name: str):
         super().__init__(name)
@@ -67,12 +194,19 @@ class Mouse(Peripheral):
         ):
             pass
         self.Disconnected = Disconnected
-        class Moved(MouseMoved, Event):
+        class Moved(MouseMoved, Event): # type: ignore
             pass
         self.Moved = Moved
 
+        class Button(PressableMouseButton, mouse=self):
+            pass
+        self.Button = Button
+
         self._position: Optional[tuple[int, int]] = None
         self._window: Optional[Window] = None
+
+    def __repr__(self) -> str:
+        return f'<gamut.peripheral.Mouse {self._name!r}>'
 
     @property
     def position(self) -> Optional[tuple[int, int]]:
