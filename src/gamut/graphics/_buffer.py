4315f2e21fd1d74156502e865d3534988d183ec2
Erik Soma <stillusingirc@gmail.com>
2021-10-17 19:36:05 -0400

    Add start of glm stubs.

---
+++ src/gamut/graphics/_buffer.py
@@ -0,0 +1,13 @@
+
+from __future__ import annotations
+
+__all__ = ['Buffer']
+
+# python
+from typing import Optional
+
+
+class Buffer:
+
+    def __init__(self, data: Optional[bytes] = None):
+        pass
