7049f4752c569975881ef2fb640bf178c9cca13d
Erik Soma <stillusingirc@gmail.com>
2021-11-12 22:51:06 -0500

    Add TransformNode.

--- src/gamut/_transformnode.py
+++ src/gamut/_transformnode.py
@@ -20,25 +20,52 @@ class TransformNode:
         self._parent = parent
         if local_transform is None:
             local_transform = mat4(1)
-        self._local_transform = local_transform
+        self._local_transform = mat4(local_transform)
+        self._parent_transform: Optional[mat4] = None
         self._transform: Optional[mat4] = None
+
+    def _check_for_cycle(self, parent: TransformNode) -> None:
+        if parent == self:
+            raise ValueError('transform parent/child relationship cycle')
+        chain = {self}
+        node: Optional[TransformNode] = parent
+        while node is not None:
+            chain.add(node)
+            node = node.parent
+            if node in chain:
+                raise ValueError('transform parent/child relationship cycle')
+
 
     @property
     def local_transform(self) -> mat4:
-        return self._local_transform
+        return mat4(self._local_transform)
 
     @local_transform.setter
     def local_transform(self, value: mat4) -> None:
-        self._transform = None
-        self._local_transform = value
+        if value != self._local_transform:
+            self._transform = None
+            self._local_transform = mat4(value)
 
     @property
     def transform(self) -> mat4:
-        if self._transform is None:
-            self._transform = self._local_transform
+        if (self._parent is not None and
+            self._parent_transform != self._parent.transform):
+            self._parent_transform = None
+
+        if (
+            self._transform is None or
+            (
+                self._parent is not None and
+                self._parent_transform is None
+            )
+        ):
+            self._transform = mat4(self._local_transform)
             if self._parent is not None:
-                self._transform @= self._parent.transform
-        return self._transform
+                if self._parent_transform is None:
+                    self._parent_transform = self._parent.transform
+                self._transform @= self._parent_transform
+
+        return mat4(self._transform)
 
     @property
     def parent(self) -> Optional[TransformNode]:
@@ -46,5 +73,8 @@ class TransformNode:
 
     @parent.setter
     def parent(self, value: TransformNode) -> None:
-        self._transform = None
-        self._parent = value
+        if value is not self._parent:
+            self._check_for_cycle(value)
+            self._transform = None
+            self._parent_transform = None
+            self._parent = value
--- test/test_transformnode.py
+++ test/test_transformnode.py
@@ -1,8 +1,12 @@
 
 # gamut
 from gamut import TransformNode
+# python
+from typing import Optional
 # pyglm
-from glm import mat4
+from glm import mat4, translate, vec3
+# pytest
+import pytest
 
 
 def test_defaults() -> None:
@@ -10,3 +14,133 @@ def test_defaults() -> None:
     assert node.local_transform == mat4(1)
     assert node.transform == mat4(1)
     assert node.parent is None
+
+
+def test_local_transform_identity() -> None:
+    local_transform = mat4(1)
+    node = TransformNode(local_transform=local_transform)
+    assert node.local_transform == local_transform
+    assert node.local_transform is not local_transform
+
+    local_transform[0][0] = 4
+    assert node.local_transform != local_transform
+    assert node.local_transform is not local_transform
+
+    local_transform = node.local_transform
+    local_transform[0][0] = 4
+    assert node.local_transform != local_transform
+    assert node.local_transform is not local_transform
+
+
+def test_transform_identity() -> None:
+    node = TransformNode()
+    transform = node.transform
+    assert node.transform is not transform
+
+    transform[0][0] = 4
+    assert node.transform != transform
+    assert node.transform is not transform
+
+
+@pytest.mark.parametrize("translation_x", [-8, 0, 7])
+@pytest.mark.parametrize("translation_y", [-8, 0, 7])
+@pytest.mark.parametrize("translation_z", [-8, 0, 7])
+def test_transform_no_parent(
+    translation_x: int,
+    translation_y: int,
+    translation_z: int,
+) -> None:
+    local_transform = translate(
+        mat4(1),
+        vec3(translation_x, translation_y, translation_z),
+    )
+    node = TransformNode(local_transform=local_transform)
+    assert node.local_transform == local_transform
+    assert node.transform == local_transform
+
+
+@pytest.mark.parametrize("translation_x", [-8, 0, 7])
+@pytest.mark.parametrize("translation_y", [-8, 0, 7])
+@pytest.mark.parametrize("translation_z", [-8, 0, 7])
+def test_transform_parent(
+    translation_x: int,
+    translation_y: int,
+    translation_z: int,
+) -> None:
+    local_transform = translate(
+        mat4(1),
+        vec3(translation_x, translation_y, translation_z),
+    )
+
+    parent = TransformNode(local_transform=local_transform)
+    node = TransformNode(local_transform=local_transform, parent=parent)
+    assert node.local_transform == local_transform
+    assert node.transform == local_transform @ local_transform
+
+
+def test_add_parent() -> None:
+    node_local_transform = translate(
+        mat4(1),
+        vec3(1, 1, 1),
+    )
+    node = TransformNode(local_transform=node_local_transform)
+    assert node.local_transform == node_local_transform
+    assert node.transform == node_local_transform
+
+    parent_local_transform = translate(
+        mat4(1),
+        vec3(-2, -2, -2),
+    )
+    parent = TransformNode(local_transform=parent_local_transform)
+    node.parent = parent
+    assert node.local_transform == node_local_transform
+    assert node.transform == node_local_transform @ parent_local_transform
+
+
+def test_remove_parent() -> None:
+    parent_local_transform = translate(
+        mat4(1),
+        vec3(-2, -2, -2),
+    )
+    parent = TransformNode(local_transform=parent_local_transform)
+
+    node_local_transform = translate(
+        mat4(1),
+        vec3(1, 1, 1),
+    )
+    node = TransformNode(local_transform=node_local_transform, parent=parent)
+    assert node.local_transform == node_local_transform
+    assert node.transform == node_local_transform @ parent_local_transform
+
+    node.parent = None
+    assert node.local_transform == node_local_transform
+    assert node.transform == node_local_transform
+
+
+def test_change_parent_local_transform() -> None:
+    parent = TransformNode()
+    node = TransformNode(parent=parent)
+
+    assert parent.transform == mat4(1)
+    assert node.transform == mat4(1)
+
+    parent.local_transform = translate(mat4(1), vec3(-2, 1, 99))
+    assert parent.transform == parent.local_transform
+    assert node.transform == parent.local_transform
+
+
+@pytest.mark.parametrize("depth", [1, 2, 4, 10])
+def test_cycle(depth: int) -> None:
+    nodes: list[TransformNode] = []
+    for _ in range(depth):
+        parent_node: Optional[TransformNode] = None
+        try:
+            parent_node = nodes[-1]
+        except IndexError:
+            pass
+        node = TransformNode(parent=parent_node)
+        nodes.append(node)
+
+    with pytest.raises(ValueError) as excinfo:
+        nodes[0].parent = nodes[-1]
+    assert str(excinfo.value) == 'transform parent/child relationship cycle'
