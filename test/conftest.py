17d0356b17f536744a73ab3e2a124dc1aaa95948
Erik Soma <stillusingirc@gmail.com>
2021-11-23 09:19:59 -0500

    Check SDL state prior to running test

--- test/conftest.py
+++ test/conftest.py
@@ -32,24 +32,14 @@ def cleanup(request: Any) -> Generator[Any, Any, None]:
     """This fixture helps to clean up any state that would persist between
     tests.
     """
-    # do the test
-    yield
-    # make sure all events are reset so that the event futures don't persist
-    # any tasks that are still waiting
-    reset_events()
-    # re-enable garbage collection in case it was disabled without re-enabling
-    gc.enable()
-    # force a garbage collection to get rid of anything that is still dangling
-    # and hasn't had its __del__ resolved
-    gc.collect()
-    # make sure SDL was completley de-initialized
-    sdl_timer_init = SDL_WasInit(SDL_INIT_TIMER) == 0
-    sdl_audio_init = SDL_WasInit(SDL_INIT_AUDIO) == 0
-    sdl_video_init = SDL_WasInit(SDL_INIT_VIDEO) == 0
-    sdl_joystick_init = SDL_WasInit(SDL_INIT_JOYSTICK) == 0
-    sdl_haptic_init = SDL_WasInit(SDL_INIT_HAPTIC) == 0
-    sdl_gamecontroller_init = SDL_WasInit(SDL_INIT_GAMECONTROLLER) == 0
-    sdl_events_init = SDL_WasInit(SDL_INIT_EVENTS) == 0
+    # make sure SDL is completley de-initialized
+    sdl_timer_init = SDL_WasInit(SDL_INIT_TIMER) != 0
+    sdl_audio_init = SDL_WasInit(SDL_INIT_AUDIO) != 0
+    sdl_video_init = SDL_WasInit(SDL_INIT_VIDEO) != 0
+    sdl_joystick_init = SDL_WasInit(SDL_INIT_JOYSTICK) != 0
+    sdl_haptic_init = SDL_WasInit(SDL_INIT_HAPTIC) != 0
+    sdl_gamecontroller_init = SDL_WasInit(SDL_INIT_GAMECONTROLLER) != 0
+    sdl_events_init = SDL_WasInit(SDL_INIT_EVENTS) != 0
     if SDL_WasInit(
         SDL_INIT_TIMER |
         SDL_INIT_AUDIO |
@@ -67,3 +57,13 @@ def cleanup(request: Any) -> Generator[Any, Any, None]:
         warnings.warn(f'{sdl_events_init=}')
         warnings.warn('SDL in unexpected state, cannot continue testing')
         sys.exit(1)
+    # do the test
+    yield
+    # make sure all events are reset so that the event futures don't persist
+    # any tasks that are still waiting
+    reset_events()
+    # re-enable garbage collection in case it was disabled without re-enabling
+    gc.enable()
+    # force a garbage collection to get rid of anything that is still dangling
+    # and hasn't had its __del__ resolved
+    gc.collect()
