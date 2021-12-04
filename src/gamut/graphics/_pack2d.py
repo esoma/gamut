996f389be247efda18f96dc33b2011dbd6fd7be0
Erik Soma <stillusingirc@gmail.com>
2021-12-04 13:02:06 -0500

    Use glm with Pack2d.

--- src/gamut/graphics/_pack2d.py
+++ src/gamut/graphics/_pack2d.py
@@ -3,26 +3,31 @@ from __future__ import annotations
 
 __all__ = ['Pack2d', 'Packed2dItem']
 
+# gamut
+from gamut._glmhelp import I32Vector2, ivec2_exact
 # python
 from typing import Optional
+# pyglm
+from glm import ivec2
 
 
 class Pack2d:
 
     def __init__(
         self,
-        bin_size: tuple[int, int],
+        bin_size: I32Vector2,
         *,
         max_bins: Optional[int] = None,
     ):
-        self._bin_size = bin_size
+        self._bin_size = ivec2_exact(bin_size)
         self._max_bins = max_bins
-        self._items: list[tuple[int, int]] = []
+        self._items: list[ivec2] = []
         self._unpacked_indexes: set[int] = set()
         self._bins: list[Bin] = []
         self._map: dict[int, Packed2dItem] = {}
 
-    def add(self, size: tuple[int, int]) -> int:
+    def add(self, size: I32Vector2) -> int:
+        size = ivec2_exact(size)
         id = len(self._items)
         self._items.append(size)
         self._unpacked_indexes.add(id)
@@ -63,7 +68,7 @@ class Pack2d:
 
 class Packed2dItem:
 
-    def __init__(self, bin: int, position: tuple[int, int]):
+    def __init__(self, bin: int, position: ivec2):
         self._bin = bin
         self._position = position
 
@@ -80,23 +85,23 @@ class Packed2dItem:
         return self._bin
 
     @property
-    def position(self) -> tuple[int, int]:
-        return self._position
+    def position(self) -> ivec2:
+        return ivec2(self._position)
 
     def __repr__(self) -> str:
         return (
             f'<gamut.graphics.Pack2dItem bin={self._bin} '
-            f'position={self._position}>'
+            f'position={tuple(self._position)}>'
         )
 
 
 class Bin:
 
-    def __init__(self, max_size: tuple[int, int]):
+    def __init__(self, max_size: ivec2):
         self.max_size = max_size
         self.lines: list[list[int]] = []
 
-    def add(self, size: tuple[int, int]) -> Optional[tuple[int, int]]:
+    def add(self, size: ivec2) -> Optional[ivec2]:
         # early out for things that are larger than the max bin size
         if size[0] > self.max_size[0] or size[1] > self.max_size[1]:
             return None
@@ -109,11 +114,11 @@ class Bin:
             # only the last line can have its height expanded
             if (i + 1) < len(self.lines) and size[1] > line_height:
                 continue
-            position = (line_width, bottom - line_height)
+            position = ivec2(line_width, bottom - line_height)
             self.lines[i] = [line_width + size[0], max(line_height, size[1])]
             return position
         # thing is too vertically large to fit in a new line
         if bottom + size[1] > self.max_size[1]:
             return None
         self.lines.append(list(size))
-        return (0, bottom)
+        return ivec2(0, bottom)
