5cbe4e6d1a98fbb9bae0704e5452f63f9b9c6779
Erik Soma <stillusingirc@gmail.com>
2021-12-07 18:07:16 -0500

    Add IndexBuffer and UniformMap aliases.

--- src/gamut/graphics/__init__.py
+++ src/gamut/graphics/__init__.py
@@ -14,6 +14,7 @@ __all__ = [
     'DepthTest',
     'execute_shader',
     'FaceCull',
+    'IndexBuffer',
     'Image',
     'ImageInvalidError',
     'MipmapSelection',
@@ -35,6 +36,7 @@ __all__ = [
     'TextureRenderTargetDepthStencil',
     'TextureView',
     'TextureWrap',
+    'UniformMap',
     'WindowRenderTarget',
 ]
 
