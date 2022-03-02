fc739da3b435ef2baea679b45c2e9fa0b9539639
Erik Soma <stillusingirc@gmail.com>
2022-03-01 20:00:02 -0500

    #

--- test/collision/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for
-# Source directory: C:/Users/erik/Documents/code/gamut/vendor/bullet3/test/collision
-# Build directory: C:/Users/erik/Documents/code/gamut/test/collision
-#
-# This file includes the relevant testing commands required for
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Test_Collision_PASS "Test_Collision")
-set_tests_properties(Test_Collision_PASS PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/erik/Documents/code/gamut/vendor/bullet3/test/collision/CMakeLists.txt;37;ADD_TEST;C:/Users/erik/Documents/code/gamut/vendor/bullet3/test/collision/CMakeLists.txt;0;")
