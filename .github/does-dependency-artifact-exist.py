d16d58263dd6b7a30ce3611435944bb1a853caf7
Erik Soma <stillusingirc@gmail.com>
2022-03-25 17:05:57 -0400

    Try building dependencies with artifacts.

---
+++ .github/does-dependency-artifact-exist.py
@@ -0,0 +1,18 @@
+
+# python
+import sys
+# requests
+import requests
+
+artifact_name = sys.argv[1]
+print(f'Looking for {artifact_name}...', file=sys.stderr)
+
+response = requests.get(
+    'https://api.github.com/repos/esoma/gamut/actions/artifacts'
+)
+for artifact in response.json()["artifacts"]:
+    if artifact["name"] == artifact_name:
+        print('Artifact already exists.', file=sys.stderr)
+        print('1')
+
+print('Artifact does not exist.', file=sys.stderr)
