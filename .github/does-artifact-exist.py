084d6972b7c50f5c89ddd067560b4debd931aeda
Erik Soma <stillusingirc@gmail.com>
2022-03-26 09:16:33 -0400

    #

--- .github/does-artifact-exist.py
+++ .github/does-artifact-exist.py
@@ -5,15 +5,25 @@ import sys
 import requests
 
 artifact_name = sys.argv[1]
+github_env = sys.argv[2]
 print(f'Looking for {artifact_name}...', file=sys.stderr)
 
 response = requests.get(
     'https://api.github.com/repos/esoma/gamut/actions/artifacts'
 )
+if response.status != 200:
+    print(
+        f'Unable to check if artifact exists: {response.status}',
+        file=sys.stderr
+    )
+    sys.exit(1)
 for artifact in response.json()["artifacts"]:
     if artifact["name"] == artifact_name:
         print('Artifact already exists.', file=sys.stderr)
-        print('1')
+        with open(github_env, 'a') as f:
+            f.write(f'ARTIFACT_EXISTS=1\n')
         sys.exit(0)
 
 print('Artifact does not exist.', file=sys.stderr)
+with open(github_env, 'a') as f:
+    f.write(f'ARTIFACT_EXISTS=0\n')
