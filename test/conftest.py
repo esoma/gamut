d29d97d35cd59832c5b80c88771290ba3064e4ea
Erik Soma <stillusingirc@gmail.com>
2022-04-01 15:53:43 -0400

    isort

--- test/conftest.py
+++ test/conftest.py
@@ -10,12 +10,12 @@ import signal
 import time
 from typing import Any, Generator
 import warnings
+# pytest
+import pytest
 # pysdl2
 from sdl2 import (SDL_INIT_AUDIO, SDL_INIT_EVENTS, SDL_INIT_GAMECONTROLLER,
                   SDL_INIT_HAPTIC, SDL_INIT_JOYSTICK, SDL_INIT_TIMER,
                   SDL_INIT_VIDEO, SDL_Quit, SDL_WasInit)
-# pytest
-import pytest
 
 
 def pytest_configure(config: Any) -> None:
