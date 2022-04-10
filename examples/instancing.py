6fbdd49f1f8889ff30e530aa6ea613e7ab9cac68
Erik Soma <stillusingirc@gmail.com>
2022-04-10 11:47:27 -0400

    Add UV generation to RectangularCuboid.render.

--- examples/instancing.py
+++ examples/instancing.py
@@ -1,10 +1,10 @@
 
 # gamut
+from gamut.geometry import RectangularCuboid
 from gamut.graphics import (BufferView, BufferViewMap, clear_render_target,
                             DepthTest, execute_shader, FaceCull, Image,
                             PrimitiveMode, Shader)
-from gamut.math import (FMatrix4, FMatrix4Array, FVector2, FVector2Array,
-                        FVector3, FVector3Array, U8Array)
+from gamut.math import FMatrix4, FMatrix4Array, FVector3
 # python
 from typing import Final
 # examples
@@ -21,28 +21,12 @@ class App(ExampleApplication):
         self.window.title = 'Gamut Instancing Example'
 
         self.shader = Shader(vertex=VERTEX_SHADER, fragment=FRAGMENT_SHADER)
+        cube = RectangularCuboid(FVector3(0), FVector3(2))
+        cube_positions, _, cube_uvs, cube_indices = cube.render()
         self.cube_transform = FMatrix4(1)
         self.cube_attributes = BufferViewMap({
-            "pos": BufferView.from_array(FVector3Array(
-                FVector3(-1, -1, -1),
-                FVector3(1, -1, -1),
-                FVector3(1, 1, -1),
-                FVector3(-1, 1, -1),
-                FVector3(-1, -1, 1),
-                FVector3(1, -1, 1),
-                FVector3(1, 1, 1),
-                FVector3(-1, 1, 1),
-            )),
-            "uv": BufferView.from_array(FVector2Array(
-                FVector2(0, 0),
-                FVector2(1, 0),
-                FVector2(1, 1),
-                FVector2(0, 1),
-                FVector2(1, 1),
-                FVector2(0, 1),
-                FVector2(0, 0),
-                FVector2(1, 0),
-            )),
+            "pos": BufferView.from_array(cube_positions),
+            "uv": BufferView.from_array(cube_uvs),
             "instance_transform": BufferView.from_array(
                 FMatrix4Array(*(
                     FMatrix4(1).translate(FVector3(x * 4, y * 4, z * -4))
@@ -53,14 +37,7 @@ class App(ExampleApplication):
                 instancing_divisor=1,
             ),
         })
-        self.cube_index_buffer_view = BufferView.from_array(U8Array(
-            0, 2, 1, 2, 0, 3,
-            4, 5, 6, 6, 7, 4,
-            1, 6, 5, 6, 1, 2,
-            0, 7, 3, 7, 0, 4,
-            0, 5, 4, 5, 0, 1,
-            3, 7, 6, 6, 2, 3,
-        ))
+        self.cube_index_buffer_view = BufferView.from_array(cube_indices)
         self.cube_texture = Image(RESOURCES / 'yee.jpg').to_texture()
 
     async def draw(self, step: ExampleApplication.Step) -> None:
