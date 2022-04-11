2bcd967a896a6a7e0d1b0ab6ea5bcc8b4ad833d3
Erik Soma <stillusingirc@gmail.com>
2022-04-11 19:39:00 -0400

    Add ShaderCodePreprocessor and JsonShaderLoader.

--- src/gamut/graphics/__init__.py
+++ src/gamut/graphics/__init__.py
@@ -16,6 +16,8 @@ __all__ = [
     'IndexBuffer',
     'Image',
     'ImageInvalidError',
+    'JsonShaderLoader',
+    'JsonFileShaderLoader',
     'MipmapSelection',
     'Pack2d',
     'Packed2dItem',
@@ -25,6 +27,7 @@ __all__ = [
     'read_stencil_from_render_target',
     'Shader',
     'ShaderAttribute',
+    'ShaderCodePreprocessor',
     'ShaderExecutionResult',
     'ShaderUniform',
     'Texture',
@@ -47,6 +50,7 @@ __all__ = [
 from ._buffer import (Buffer, BufferFrequency, BufferNature, BufferView,
                       BufferViewMap)
 from ._image import Image, ImageInvalidError
+from ._jsonshaderloader import JsonFileShaderLoader, JsonShaderLoader
 from ._pack2d import Pack2d, Packed2dItem
 from ._quad import create_quad_position_array, create_quad_uv_array
 from ._rendertarget import (clear_render_target, read_color_from_render_target,
@@ -59,6 +63,7 @@ from ._shader import (BlendFactor, BlendFunction, DepthTest, execute_shader,
                       FaceCull, IndexBuffer, PrimitiveMode, Shader,
                       ShaderAttribute, ShaderExecutionResult, ShaderUniform,
                       UniformMap)
+from ._shadercodepreprocessor import ShaderCodePreprocessor
 from ._texture2d import Texture2d, Texture2dArray
 from ._texture import (MipmapSelection, Texture, TextureComponents,
                        TextureDataType, TextureFilter, TextureType,
---
+++ test/graphics/test_shadercodepreprocessor.py
@@ -0,0 +1,127 @@
+
+# gamut
+from gamut.graphics import ShaderCodePreprocessor
+# python
+from textwrap import dedent
+# pytest
+import pytest
+
+
+def test_noop() -> None:
+    scp = ShaderCodePreprocessor()
+    input_code = dedent('''
+    #version 140
+    in vec3 pos;
+    in vec2 uv;
+    out vec2 vertex_uv;
+    uniform mat4 camera_transform;
+    uniform mat4 model_transform;
+    void main()
+    {
+        vertex_uv = uv;
+        gl_Position = camera_transform * model_transform * vec4(pos, 1.0);
+    }
+    ''').encode('utf-8')
+    output_code = scp(input_code)
+    assert output_code == input_code
+
+
+def test_defines() -> None:
+    scp = ShaderCodePreprocessor()
+    input_code = dedent('''
+    #version 140
+    in vec3 pos;
+    in vec2 uv;
+    out vec2 vertex_uv;
+    uniform mat4 camera_transform;
+    uniform mat4 model_transform;
+    void main()
+    {
+        vertex_uv = uv;
+        gl_Position = camera_transform * model_transform * vec4(pos, 1.0);
+    }
+    ''').encode('utf-8')
+    output_code = scp(input_code, defines={
+        "def1": 1,
+        "def2": 'abc',
+    })
+    assert output_code == dedent('''
+    #version 140
+    #define def1 1
+    #define def2 abc
+    in vec3 pos;
+    in vec2 uv;
+    out vec2 vertex_uv;
+    uniform mat4 camera_transform;
+    uniform mat4 model_transform;
+    void main()
+    {
+        vertex_uv = uv;
+        gl_Position = camera_transform * model_transform * vec4(pos, 1.0);
+    }
+    ''').encode('utf-8')
+
+
+def test_include() -> None:
+    class ShaderCodePreprocessorInclude(ShaderCodePreprocessor):
+        def include(self, name: str) -> bytes:
+            if name == "first.glsl":
+                return b'// first glsl'
+            elif name == "second.glsl":
+                return b'// second glsl'
+            assert False
+
+    scp = ShaderCodePreprocessorInclude()
+    input_code = dedent('''
+    #version 140
+    in vec3 pos;
+    #include <first.glsl>
+    in vec2 uv;
+    #include "second.glsl"
+    out vec2 vertex_uv;
+    uniform mat4 camera_transform;
+    uniform mat4 model_transform;
+    void main()
+    {
+        vertex_uv = uv;
+        gl_Position = camera_transform * model_transform * vec4(pos, 1.0);
+    }
+    ''').encode('utf-8')
+    output_code = scp(input_code)
+    assert output_code == dedent('''
+    #version 140
+    in vec3 pos;
+    // first glsl
+    in vec2 uv;
+    // second glsl
+    out vec2 vertex_uv;
+    uniform mat4 camera_transform;
+    uniform mat4 model_transform;
+    void main()
+    {
+        vertex_uv = uv;
+        gl_Position = camera_transform * model_transform * vec4(pos, 1.0);
+    }
+    ''').encode('utf-8')
+
+
+def test_default_include() -> None:
+    scp = ShaderCodePreprocessor()
+    input_code = dedent('''
+    #version 140
+    in vec3 pos;
+    #include <first.glsl>
+    in vec2 uv;
+    #include "second.glsl"
+    out vec2 vertex_uv;
+    uniform mat4 camera_transform;
+    uniform mat4 model_transform;
+    void main()
+    {
+        vertex_uv = uv;
+        gl_Position = camera_transform * model_transform * vec4(pos, 1.0);
+    }
+    ''').encode('utf-8')
+    with pytest.raises(RuntimeError) as ex:
+        output_code = scp(input_code)
+    assert str(ex.value) == 'include not allowed'
